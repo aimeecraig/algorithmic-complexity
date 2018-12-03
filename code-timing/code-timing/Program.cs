63c74cb49949f0deaa9aa3157b60e07fbd151f62
Aimée Craig <aimeecraig1@gmail.com>
2018-12-03 13:58:22 +0000

    asp.net console application added

---
+++ code-timing/code-timing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace code_timing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
