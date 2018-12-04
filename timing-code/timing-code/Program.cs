e89be1f8027d601a5736c76d82ee4858e2d28843
Aimée Craig <aimeecraig1@gmail.com>
2018-12-04 16:56:52 +0000

    merge bugs fixed and readme updated

--- timing-code/timing-code/Program.cs
+++ timing-code/timing-code/Program.cs
@@ -7,10 +7,30 @@ namespace timing_code
     {
         static void Main(string[] args)
         {
+
+        }
+
+        public static string Method1(string mystring)
+        {
+            return $"{mystring} is ok";
+        }
+
+        public static int Method2(string mystring)
+        {
+            return 2;
+        }
+
+        public static void RunTheMethod(Action myMethodName)
+        {
+            myMethodName();
+        }
+
+        static string Timer(Func<string, string> TestMethod)
+        {
             Stopwatch stopWatch = Stopwatch.StartNew();
 
             // Begin testing code
-            Console.WriteLine("Hello World!");
+            string test = TestMethod("Aimee");
             // End testing code
 
             stopWatch.Stop();
@@ -19,5 +39,25 @@ namespace timing_code
 
             Console.WriteLine($"Ran in {ts.TotalMilliseconds} milliseconds.");
         }
+
+        for (int i = 1; 1 < length; i++) {
+        
+}
+ 
+        //static string RunMethod(Func<string, string> MethodName, string name)
+        //{
+        //    string result = MethodName(name);
+        //    return result;
+        //}
+
+        //static string Test(string test, string name)
+        //{
+        //    return RunMethod(test(name));
+        //}
+
+        //static string HelloWorld(string name)
+        //{
+        //    return $"Hello {name}!";
+        //}
     }
 }
